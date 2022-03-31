c788d49e01a17af105620d0f6b96aee61f94b6c5
yibenwulong <1211198696@qq.com>
2022-03-31 09:16:56 +0000

    r

--- 01_dart_start.dart
+++ 01_dart_start.dart
@@ -14,7 +14,138 @@ void variables() {
 }
 
 void main(List<String> args) {
-  // 变量
-  variables();
+  //流程控制语句
+  if (year >= 2001) {
+  print('21st century');
+} else if (year >= 1901) {
+  print('20th century');
 }
+
+for (final object in flybyObjects) {
+  print(object);
+}
+
+for (int month = 1; month <= 12; month++) {
+  print(month);
+}
+
+while (year < 2016) {
+  year += 1;
+}
+//int fibonacci(int n) {
+  if (n == 0 || n == 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+  //=> (胖箭头) 简写语法用于仅包含一条语句的函数。该语法在将匿名函数作为参数传递时非常有用：
+  flybyObjects.where((name) => name.contains('turn')).forEach(print);
+}
+
+var result = fibonacci(20);
+//导入
+  // Importing core libraries
+import 'dart:math';
+
+// Importing libraries from external packages
+import 'package:test/test.dart';
+
+// Importing files
+import 'path/to/my_other_file.dart';
+//类
+class Spacecraft {
+  String name;
+  DateTime? launchDate;
+
+  // Read-only non-final property
+  int? get launchYear => launchDate?.year;
+
+  // Constructor, with syntactic sugar for assignment to members.
+  Spacecraft(this.name, this.launchDate) {
+    // Initialization code goes here.
+  }
+
+  // Named constructor that forwards to the default one.
+  Spacecraft.unlaunched(String name) : this(name, null);
+
+  // Method.
+  void describe() {
+    print('Spacecraft: $name');
+    // Type promotion doesn't work on getters.
+    var launchDate = this.launchDate;
+    if (launchDate != null) {
+      int years = DateTime.now().difference(launchDate).inDays ~/ 365;
+      print('Launched: $launchYear ($years years ago)');
+    } else {
+      print('Unlaunched');
+    }
+  }
+}
+//使用 Spacecraft 类：
+var voyager = Spacecraft('Voyager I', DateTime(1977, 9, 5));
+voyager.describe();
+
+var voyager3 = Spacecraft.unlaunched('Voyager III');
+voyager3.describe();
+//扩展类（继承）
+class Orbiter extends Spacecraft {
+  double altitude;
+
+  Orbiter(String name, DateTime launchDate, this.altitude)
+      : super(name, launchDate);
+}
+//Mixins
+//Mixin 是一种在多个类层次结构中重用代码的方法。下面的是声明一个 Mixin 的做法：
+mixin Piloted {
+  int astronauts = 1;
+
+  void describeCrew() {
+    print('Number of astronauts: $astronauts');
+  }
+}
+//现在你只需使用 Mixin 的方式继承这个类就可将该类中的功能添加给其它类。
+
+class PilotedCraft extends Spacecraft with Piloted {
+  // ···
+}
+//接口和抽象类
+class MockSpaceship implements Spacecraft {
+  // ···
+}
+//创建一个被任意具体类扩展（或实现）的抽象类。抽象类可以包含抽象方法（不含方法体的方法）。
+
+abstract class Describable {
+  void describe();
+
+  void describeWithEmphasis() {
+    print('=========');
+    describe();
+    print('=========');
+  }
+}
+//异步
+const oneSecond = Duration(seconds: 1);
+// ···
+Future<void> printWithDelay(String message) async {
+  await Future.delayed(oneSecond);
+  print(message);
+}
+//异常
+//使用 throw 关键字抛出一个异常：
+
+if (astronauts == 0) {
+  throw StateError('No astronauts.');
+}
+//使用 try 语句配合 on 或 catch（两者也可同时使用）关键字来捕获一个异常:
+
+try {
+  for (final object in flybyObjects) {
+    var description = await File('$object.txt').readAsString();
+    print(description);
+  }
+} on IOException catch (e) {
+  print('Could not describe object: $e');
+} finally {
+  flybyObjects.clear();
+}
+}
+
+
 
